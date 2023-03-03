98e4cda6a9e7bc704efc24a43ec3b7bbbe4def98
hitch909 <evgenij909@yandex.ru>
2023-03-03 12:07:38 +0500

    программа

--- prog001/Program.cs
+++ prog001/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿//Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+
+
+int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+string str = " ";
+
+foreach (int n in array)
+    str += n.ToString();
+
+Console.WriteLine(string.Format("{0:(###) ###-####}", Convert.ToInt64(str)));
